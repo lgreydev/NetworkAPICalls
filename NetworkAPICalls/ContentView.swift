7bae6e2616c96d7b23d9e38f9c7fd000ef5dc404
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-05-31 13:56:55 -0400

    Add ViewModel, get data

--- NetworkAPICalls/ContentView.swift
+++ NetworkAPICalls/ContentView.swift
@@ -26,6 +26,31 @@ struct ContentView: View {
         }
         .padding()
     }
+    
+    func getUser() async throws -> GitHubUser {
+        let endpoint = "https://api.github.com/users/lgreydev"
+        
+        guard let url = URL(string: endpoint) else {
+            throw GitHubError.invalidURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw GitHubError.invalidResponse
+        }
+        
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(GitHubUser.self, from: data)
+        } catch {
+            throw GitHubError.invalidData
+        }
+        
+        
+    }
 }
 
 struct GitHubUser: Codable {
@@ -38,4 +63,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+enum GitHubError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
 }
