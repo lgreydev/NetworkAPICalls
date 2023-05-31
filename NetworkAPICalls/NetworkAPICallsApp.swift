63663dd5de2460146efb038eb8163b12c63c32bf
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-05-31 13:18:13 -0400

    Initial Commit

---
+++ NetworkAPICalls/NetworkAPICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkAPICallsApp.swift
+//  NetworkAPICalls
+//
+//  Created by Sergey Lukaschuk on 2023-05-31.
+//
+
+import SwiftUI
+
+@main
+struct NetworkAPICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
