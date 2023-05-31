85470c121b8db4fa2ee0f722ee784e7597a648ad
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-05-31 13:28:51 -0400

    UI user screen

--- NetworkAPICalls/ContentView.swift
+++ NetworkAPICalls/ContentView.swift
@@ -9,11 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack (spacing: 20){
+            Circle()
+                .foregroundColor(.secondary)
+                .frame(width: 120, height: 120)
+            
+            Text("User Name")
+                .bold()
+                .font(.title3)
+            
+            Text("Bio")
+                .font(.footnote)
+            
+            Spacer()
+            
         }
         .padding()
     }
